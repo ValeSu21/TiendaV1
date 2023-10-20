6b8108d1186907a0b1f1b828a7173033dcc6a3e2
valer <valer@LAPTOP-1DHQVMCL>
2023-10-20 01:59:07 -0600

    Semana 05 MySQL

---
+++ src/main/java/com/tienda_v1/controller/CategoriaController.java
@@ -0,0 +1,25 @@
+
+package com.tienda_v1.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.tienda_v1.service.CategoriaService;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        model.addAttribute("totalCategorias", categorias.size());
+
+        return "categoria/listado";
+    }
+}
